28ee9fccfc6c6f49a79be8772c042ba00a6337c5
Dan Barton <danbarton2000@gmail.com>
2017-09-16 18:19:42 +0100

    QueryRange Method
    Add the query range method which finds all points in a range

--- ComputingProject/QuadTree.cs
+++ ComputingProject/QuadTree.cs
@@ -8,7 +8,7 @@ namespace ComputingProject.Collision
 {
     class QuadTree
     {
-        int maxNodeCount = 4;
+        readonly int maxNodeCount = 4;
         
         public AABB Boundary { get; }
 
@@ -18,5 +18,49 @@ namespace ComputingProject.Collision
         QuadTree northEast;
         QuadTree southWest;
         QuadTree southEast;
+
+        QuadTree() {
+            points = new Vector[maxNodeCount];
+        }
+
+        bool Insert(Vector point) {
+            if (!Boundary.ContainsPoint(point)) {
+                return false;
+            }
+
+            for (int i = 0; i < points.Length; i++) {
+                if (points[i] == null) {
+                    points[i] = point;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        List<Vector> QueryRange(AABB range) {
+            List<Vector> pointsInRange = new List<Vector>();
+
+            if (!Boundary.IntersectsAABB(range)) {
+                return pointsInRange;
+            }
+
+            for (int i = 0; i < points.Length; i++) {
+                if (range.ContainsPoint(points[i])) {
+                    pointsInRange.Add(points[i]);
+                }
+            }
+
+            if (northWest == null) {
+                return pointsInRange;
+            }
+
+            pointsInRange.AddRange(northWest.QueryRange(range));
+            pointsInRange.AddRange(northEast.QueryRange(range));
+            pointsInRange.AddRange(southWest.QueryRange(range));
+            pointsInRange.AddRange(southEast.QueryRange(range));
+
+            return pointsInRange;
+        }
     }
 }
