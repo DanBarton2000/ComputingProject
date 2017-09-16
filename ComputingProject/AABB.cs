a5cd97d6e7ef79eac25c555f758e2673713f411f
Dan Barton <danbarton2000@gmail.com>
2017-09-16 17:48:53 +0100

    AABB Struct
    Added the AABB struct with a complete contains point method and an incomplete intersects AABB method

---
+++ ComputingProject/AABB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputingProject.Collision
+{
+    struct AABB
+    {
+        Vector centre;
+        double halfDimension;
+
+        public AABB(Vector centre, double halfDimension) {
+            this.centre = centre;
+            this.halfDimension = halfDimension;
+        }
+
+        public bool ContainsPoint(Vector point) {
+            double minX = centre.x - halfDimension;
+            double minY = centre.y - halfDimension;
+            double maxX = centre.x + halfDimension;
+            double maxY = centre.y + halfDimension;
+
+            if (point.x < minX || point.x > maxX || point.y < minY || point.y > maxY) {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IntersectsAABB(AABB box) {
+            return false;
+        }
+    }
+}
