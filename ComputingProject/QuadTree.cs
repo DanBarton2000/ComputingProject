079b53644e24aca3ab81b1fc99258440f523c91f
Dan Barton <danbarton2000@gmail.com>
2017-09-16 17:54:01 +0100

    Quadtree Class

---
+++ ComputingProject/QuadTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputingProject.Collision
+{
+    class QuadTree
+    {
+        int maxNodeCount = 4;
+        
+        public AABB Boundary { get; }
+
+        Vector[] points;
+
+        QuadTree northWest;
+        QuadTree northEast;
+        QuadTree southWest;
+        QuadTree southEast;
+    }
+}
