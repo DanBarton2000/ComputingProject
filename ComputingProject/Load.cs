092b9f7896dee4874b6e17cbef20bb18f6f022b4
Dan Barton <danbarton2000@gmail.com>
2017-11-09 13:47:28 +0000

    Load class
    Read XML and return the objects

---
+++ ComputingProject/Load.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ComputingProject {
+    public class Load {
+        public List<CelestialObject> ReadXML(string filename) {
+            try {
+                XmlSerializer reader = new XmlSerializer(typeof(CelestialObject));
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + filename;
+                StreamReader file = new StreamReader(path);
+                List<CelestialObject> objects = (List<CelestialObject>)reader.Deserialize(file);
+                return objects;
+            }
+            catch(FileNotFoundException) {
+                Console.WriteLine("File not found!");
+                return null;
+            }
+
+        }
+    }
+}
