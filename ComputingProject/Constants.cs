ffdc13a623a4504723d44a27103a76a5698e46b9
DanBarton2000 <danbarton2000@gmail.com>
2018-02-08 09:04:56 +0000

    Changed solar mass from 3 sig figs to 4 sig figs

--- ComputingProject/Constants.cs
+++ ComputingProject/Constants.cs
@@ -9,7 +9,7 @@ namespace ComputingProject
     public class Constants
     {
         public static readonly double SolarLuminosity = 3.846E+26;
-        public static readonly double SolarMass = 1.99E+30;
+        public static readonly double SolarMass = 1.989E+30;
         public static readonly double AstronomicalUnit = 149597870700;
         public static readonly double Gravitational = 6.67408E-11;
         public static readonly double DegreesToRadians = Math.PI / 180;
